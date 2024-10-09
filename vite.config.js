4bbba7aaaab4d71d408395333723d32c76c49380
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2024-10-09 08:37:57 +0200

    render test 1

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public' // Cambia 'dist' por 'public'
+  }
+  // base:"/project-promo-u-module-3-team-3/",
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:"/project-promo-u-module-3-team-3/",
-})
->>>>>>> 84d8e0b330430488b98b44ed329c40e0e01e6aa6
